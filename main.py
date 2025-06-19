690bbbf310a211732f02f37cf7f02f2a359d6ca3
Gin <86393880+zzfz1@users.noreply.github.com>
2025-06-19 17:10:55 +0800

    Handle Lightning API changes in CUDA callback

--- main.py
+++ main.py
@@ -425,18 +425,34 @@ class CUDACallback(Callback):
     # see https://github.com/SeanNaren/minGPT/blob/master/mingpt/callback.py
     def on_train_epoch_start(self, trainer, pl_module):
         # Reset the memory use counter
-        torch.cuda.reset_peak_memory_stats(trainer.root_gpu)
-        torch.cuda.synchronize(trainer.root_gpu)
+        if hasattr(trainer, "root_gpu"):
+            device_index = trainer.root_gpu
+        elif getattr(trainer, "strategy", None) is not None:
+            device_index = trainer.strategy.root_device.index
+        else:
+            device_index = 0
+        torch.cuda.reset_peak_memory_stats(device_index)
+        torch.cuda.synchronize(device_index)
         self.start_time = time.time()
 
     def on_train_epoch_end(self, trainer, pl_module, outputs):
-        torch.cuda.synchronize(trainer.root_gpu)
-        max_memory = torch.cuda.max_memory_allocated(trainer.root_gpu) / 2 ** 20
+        if hasattr(trainer, "root_gpu"):
+            device_index = trainer.root_gpu
+        elif getattr(trainer, "strategy", None) is not None:
+            device_index = trainer.strategy.root_device.index
+        else:
+            device_index = 0
+        torch.cuda.synchronize(device_index)
+        max_memory = torch.cuda.max_memory_allocated(device_index) / 2 ** 20
         epoch_time = time.time() - self.start_time
 
         try:
-            max_memory = trainer.training_type_plugin.reduce(max_memory)
-            epoch_time = trainer.training_type_plugin.reduce(epoch_time)
+            if hasattr(trainer, "training_type_plugin"):
+                max_memory = trainer.training_type_plugin.reduce(max_memory)
+                epoch_time = trainer.training_type_plugin.reduce(epoch_time)
+            elif hasattr(trainer, "strategy"):
+                max_memory = trainer.strategy.reduce(max_memory)
+                epoch_time = trainer.strategy.reduce(epoch_time)
 
             rank_zero_info(f"Average Epoch time: {epoch_time:.2f} seconds")
             rank_zero_info(f"Average Peak memory {max_memory:.2f}MiB")
